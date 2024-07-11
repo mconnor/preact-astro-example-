96485e6ed67ad4625c218d7e55e44b4afed6d58f
mike connor <mike@mikeconnor.tech>
2024-07-11 11:05:29 -0500

    tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   // Enable Preact to support Preact JSX components.
-  integrations: [preact()],
+  integrations: [preact({ devtools: true })],
+  output: 'static',
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly ESLINT_USE_FLAT_CONFIG: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
